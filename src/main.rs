226a743836d3cdeaa7a9529c352fb5bf07e7d711
Nathaniel R. Lewis <linux.robotdude@gmail.com>
2021-08-10 17:15:56 -0700

    add instruction set enums

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::rc::Rc;
 
 enum AddressSpace {
     Code,
-    Data
+    Data,
 }
 
 trait Memory {
@@ -13,13 +13,13 @@ trait Memory {
 }
 
 struct ROM {
-    data: Vec<u8>
+    data: Vec<u8>,
 }
 
 impl ROM {
     fn load_from_binary(path: &Path) -> Result<ROM, Box<dyn std::error::Error + 'static>> {
         Ok(ROM {
-            data: fs::read(path)?
+            data: fs::read(path)?,
         })
     }
 }
@@ -39,16 +39,14 @@ impl Memory for ROM {
 }
 
 struct RAM {
-    data: Vec<u8>
+    data: Vec<u8>,
 }
 
 impl RAM {
     fn create_with_size(size: usize) -> RAM {
         let mut data = Vec::with_capacity(size);
         data.resize(size, 0);
-        RAM {
-            data: data
-        }
+        RAM { data: data }
     }
 }
 
@@ -61,43 +59,119 @@ impl Memory for RAM {
     }
 }
 
-struct MemoryMapperModule<'a, A : Memory, B : Memory> {
-    rom: &'a mut A,
-    ram: &'a mut B
+struct MemoryMapperModule<A: Memory, B: Memory> {
+    rom: Rc<A>,
+    ram: Rc<B>,
 }
 
-impl <'a, A : Memory, B : Memory> MemoryMapperModule<'a, A, B> {
-    fn new(rom: &'a mut A, ram: &'a mut B) -> MemoryMapperModule<'a, A, B> {
-        MemoryMapperModule {
-            rom: rom,
-            ram: ram
+impl<A: Memory, B: Memory> MemoryMapperModule<A, B> {
+    fn new(rom: Rc<A>, ram: Rc<B>) -> MemoryMapperModule<A, B> {
+        MemoryMapperModule { rom: rom, ram: ram }
+    }
+}
+
+impl<A: Memory, B: Memory> Memory for MemoryMapperModule<A, B> {
+    fn read_memory(&mut self, space: AddressSpace, address: usize) -> u8 {
+        match space {
+            AddressSpace::Code => Rc::get_mut(&mut self.rom)
+                .unwrap()
+                .read_memory(AddressSpace::Data, address),
+            AddressSpace::Data => Rc::get_mut(&mut self.ram)
+                .unwrap()
+                .read_memory(AddressSpace::Data, address),
         }
     }
+    fn write_memory(&mut self, _space: AddressSpace, _address: usize, _data: u8) {}
 }
 
-impl <'a, A : Memory, B : Memory> Memory for MemoryMapperModule<'a, A, B> {
+struct SelfProgramModule<A: Memory, B: Memory> {
+    rom: Rc<A>,
+    ram: Rc<B>,
+}
+
+impl<A: Memory, B: Memory> SelfProgramModule<A, B> {
+    fn new(rom: Rc<A>, ram: Rc<B>) -> SelfProgramModule<A, B> {
+        SelfProgramModule { rom: rom, ram: ram }
+    }
+}
+
+impl<A: Memory, B: Memory> Memory for SelfProgramModule<A, B> {
     fn read_memory(&mut self, space: AddressSpace, address: usize) -> u8 {
-        0
+        match space {
+            AddressSpace::Code => Rc::get_mut(&mut self.rom)
+                .unwrap()
+                .read_memory(AddressSpace::Data, address),
+            AddressSpace::Data => Rc::get_mut(&mut self.ram)
+                .unwrap()
+                .read_memory(AddressSpace::Data, address),
+        }
     }
-    fn write_memory(&mut self, space: AddressSpace, address: usize, data: u8) {
-
-    }
+    fn write_memory(&mut self, _space: AddressSpace, _address: usize, _data: u8) {}
 }
 
-struct CPU8051<'a> {
-
+enum ISA8051 {
+    ACALL,
+    ADD,
+    ADDC,
+    AJMP,
+    ANL,
+    CJNE,
+    CLR,
+    CPL,
+    DA,
+    DEC,
+    DIV,
+    DJNZ,
+    INC,
+    JB,
+    JBC,
+    JC,
+    JMP,
+    JNB,
+    JNC,
+    JNZ,
+    JZ,
+    LCALL,
+    LJMP,
+    MOV,
+    MOVC,
+    MOVX,
+    MUL,
+    NOP,
+    ORL,
+    POP,
+    PUSH,
+    RET,
+    RETI,
+    RL,
+    RLC,
+    RR,
+    RRC,
+    SETB,
+    SJMP,
+    SUBB,
+    SWAP,
+    XCH,
+    XCHD,
+    XRL,
+    Undefined
 }
+
+struct CPU8051 {}
 
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     // load the application rom
     let rom_path = Path::new("rom.bin");
-    let mut rom = ROM::load_from_binary(rom_path)?;
+    let rom = Rc::new(ROM::load_from_binary(rom_path)?);
 
     // create a ram for the application
-    let mut ram = RAM::create_with_size(8192);
+    let ram = Rc::new(RAM::create_with_size(8192));
 
     // create the memory mapper
-    let mut mmu = MemoryMapperModule::new(&mut rom, &mut ram);
+    let mut mmu = MemoryMapperModule::new(rom, ram);
+
+    println!("rom:0 = {:x}", mmu.read_memory(AddressSpace::Code, 0));
+    println!("ram:0 = {:x}", mmu.read_memory(AddressSpace::Data, 0));
 
     Ok(())
 }
